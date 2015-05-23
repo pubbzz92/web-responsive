542c3d7dfd51b750e358c269dfce842be65d7cbe
pubbzz92 <pubbzz92@gmail.com>
2015-05-23 14:44:08 +0530

    kandy files

---
+++ JS/myjq.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $("#vid").click(function(){
+        $("#j").fadeToggle();
+
+
+    });
+});
+
+    $(document).ready(function(){
+        $("div").scroll(function(){
+            $("span").text( x+= 1);
+        });
+    });
+
+
+
+
+
